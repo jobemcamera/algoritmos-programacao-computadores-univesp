85fb229532b988dbe2ae09a0df9a2998597e049b
jobemcamera <jobecamera@gmail.com>
2023-02-20 20:58:28 -0300

    atribuição de variável e cálculo algébrico

---
+++ Algoritmos e Programação de Computadores I/Semana 02/exericio03.py
@@ -0,0 +1,11 @@
+# Escreva instruções Python que correspondem às ações a seguir e execute-as:
+# (a) Atribua o valor inteiro 3 à variável a.
+# (b) Atribua 4 à variável b.
+# (c) Atribua à variável c o valor da expressão a * a + b * b.
+
+a = 3
+b = 4
+c = a * a + b * b
+print("Valor de a =",a)
+print("Valor de b =",b)
+print("Valor de c =",c)
