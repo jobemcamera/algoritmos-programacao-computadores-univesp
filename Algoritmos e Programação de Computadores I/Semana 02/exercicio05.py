05335fb31e659e2cf8f1dddf19951e6ba644fbb3
jobemcamera <jobecamera@gmail.com>
2023-02-20 21:10:13 -0300

    indexação de strings

---
+++ Algoritmos e Programação de Computadores I/Semana 02/exercicio05.py
@@ -0,0 +1,18 @@
+# Exercício 2.5
+# Comece executando a atribuição:
+# s = '0123456789'
+# Agora, escreva expressões usando a string s e o operador de indexação que é avaliado como:
+# (a) '0'
+# (b) '1'
+# (c) '6'
+# (d) '8'
+# (e) '9'
+
+s = '0123456789'
+print("String:", s)
+print("A posição 0 é: ", s[0])
+print("A posição 1 é: ", s[1])
+print("A posição 6 é: ", s[6])
+print("A posição 8 é: ", s[8])
+print("A posição 9 é: ", s[9])
+
