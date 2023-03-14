ee88fc64c730bd76e400fb9e30fb7b10f4faef23
jobemcamera <jobecamera@gmail.com>
2023-03-14 19:45:19 -0300

    trabalhando com for para números inteiros

---
+++ Algoritmos e Programação de Computadores I/Semana 06/exercicio02.py
@@ -0,0 +1,15 @@
+"""
+Escreva o laço for que exibirá as sequências de números a seguir, um por linha, no shell interativo do Python.
+(a) Inteiros de 0 a 9 (isto é, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
+(b) Inteiros de 0 a 1 (isto é, 0, 1).
+"""
+
+# (a)
+for i in range(0, 10, 1):
+    print(i)
+
+print("")
+
+# (b)
+for j in range(0, 2, 1):
+    print(j)
