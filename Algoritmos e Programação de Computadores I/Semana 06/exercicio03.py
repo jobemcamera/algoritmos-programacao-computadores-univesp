cd3b19c30d7b8bedf60fd61be064d23cc2001873
jobemcamera <jobecamera@gmail.com>
2023-03-14 19:52:08 -0300

    trabalhando com for para exibir números

---
+++ Algoritmos e Programação de Computadores I/Semana 06/exercicio03.py
@@ -0,0 +1,29 @@
+"""
+Escreva um laço for que exiba a seguinte sequência de números, um por linha.
+(a) Inteiros de 3 até 12, inclusive este.
+(b) Inteiros de 0 até (mas não incluindo) 9, com um passo de 2 em vez do padrão 1 (isto é, 0, 2, 4, 6, 8).
+(c) Inteiros de 0 até (mas não incluindo) 24, com um passo de 3.
+(d) Inteiros de 3 até (mas não incluindo) 12, com um passo de 5.
+"""
+
+# (a)
+for i in range(3, 13, 1):
+    print(i)
+
+print("")
+
+# (b)
+for j in range(0, 9, 2):
+    print(j)
+
+print("")
+
+# (c)
+for m in range(0, 24, 3):
+    print(m)
+
+print("")
+
+# (d)
+for n in range(3, 12, 5):
+    print(n)
