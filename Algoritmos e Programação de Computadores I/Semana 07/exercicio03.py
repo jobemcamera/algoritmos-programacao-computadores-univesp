13b6dc4a02182ed489cf6f99804c3817de0e9f6f
jobemcamera <jobecamera@gmail.com>
2023-03-26 17:48:29 -0300

    escreva expressões (usando s e o operador de indexação)

---
+++ Algoritmos e Programação de Computadores I/Semana 07/exercicio03.py
@@ -0,0 +1,28 @@
+"""
+Exercício 4.1
+Comece executando a atribuição:
+s = '0123456789'
+Agora, escreva expressões (usando s e o operador de indexação) que sejam avaliadas como:
+(a) '234'
+(b) '78'
+(c) '1234567'
+(d) '0123'
+(e) '789'
+"""
+
+s = "0123456789"
+
+# (a)
+print(s[2:5])
+
+# (b)
+print(s[7:9])
+
+# (c)
+print(s[1:8])
+
+# (d)
+print(s[0:4])
+
+# (e)
+print(s[7:10])
