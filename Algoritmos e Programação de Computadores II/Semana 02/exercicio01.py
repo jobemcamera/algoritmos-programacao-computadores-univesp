7c6e601bac0606b1e4687afefe2df5fcd2858482
jobemcamera <jobecamera@gmail.com>
2023-04-17 19:47:48 -0300

    sobrecarga de operadores __add__

--- Algoritmos e Programação de Computadores II/Semana 02/exercicio01.py
+++ Algoritmos e Programação de Computadores II/Semana 02/exercicio01.py
@@ -19,18 +19,30 @@ class Point:
 
     def __repr__(self):
         return '(' + str(self.x) + ',' + str(self.y) + ')'
+    
+    # x + y equivale a x.__add__(y)
+    # x + y ==> (2,3) + (2,2) => (4,5)
+    # x + 8 ==> (2,3) + 8 => (10,11)
+    def __add__(self, other):
+        if type(other) == Point:
+            return Point(self.x + other.x, self.y + other.y)
+        else:
+            return Point(self.x + other, self.y + other)
+        
 
 # instanciando a classe Point() criando o objeto "p"
 p = Point()
-print(p)
+print('p:',p)
 
 q = Point(3,4)
-print(q)
+print('q:',q)
 q.move(3,-5)
-print(q)
+print('q.move(3,-5):',q)
 
 g = Point(1,1)
-print(g)
+print('g:',g)
 g.get()
-print(g)
+print('g.get():', g)
 
+print('p+q:',p+q)
+print('p+8:',p+8)
