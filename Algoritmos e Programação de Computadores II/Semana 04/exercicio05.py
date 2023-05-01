38cf0b4161b32d7ebcf18643cab7a625f1593f11
jobemcamera <jobecamera@gmail.com>
2023-05-01 15:42:59 -0300

    Conceitos de listas sequencial x encadeada

---
+++ Algoritmos e Programação de Computadores II/Semana 04/exercicio05.py
@@ -0,0 +1,8 @@
+# Listas
+# Lista Sequencial x Encadeada (ponteiros)
+
+l = [1, 2, 13, 1213, 53]
+for i in range(len(l)):
+    print(l[i]) # podemos acessar os elementos diretamente pelos índices
+
+# Por padrão, em Python é utilizado lista sequencial de abordagem dinâmica
